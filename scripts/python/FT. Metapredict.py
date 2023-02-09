b25d2c435afcf6d19568ffcc690c0bd5188c0df2
Cesar Ponce-Diego <73264249+Kaz-bits@users.noreply.github.com>
2023-02-09 16:17:52 -0600

    Update FT. Metapredict.py

--- scripts/python/FT. Metapredict.py
+++ scripts/python/FT. Metapredict.py
@@ -13,9 +13,6 @@ output_file="METAPREDICT_RESULTS/Proteome_disor_META.csv")
 
 # Análisis de biosensores (21) valores de pLDDT----
 
-# Emplear el archivo fasta IDRs_99p. 
-os.chdir("DATA/DATA FOR PYTHON/FASTA_META/") #nuevo directorio
-
 # Obtener los valores de pLDDT de AlphaFold con el archivo
 meta.predict_pLDDT_fasta(filepath="IDRs_99p_FASTA.fasta", 
 output_file="METAPREDICT_RESULTS/IDRs_99p_pLDDT.txt")
